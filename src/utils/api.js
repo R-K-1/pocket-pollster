a84887d0cc2fe5bddbbc3e6de1284d6a1a0804a4
Raymond Kalonji <raymondkalonji@gmail.com>
2020-05-29 21:51:54 -0400

    feat: adding API for interacting with backend

---
+++ src/utils/api.js
@@ -0,0 +1,12 @@
+import {
+    _getUsers,
+    _getQuestions
+  } from './_DATA.js'
+  
+  export function getInitialData () {
+    return Promise.all([
+      _getUsers()
+    ]).then((users) => ({
+      users
+    }))
+  }
