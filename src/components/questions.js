89c037b2a5ab2177a798d80b91b11a63775782bf
Raymond Kalonji <raymondkalonji@gmail.com>
2020-06-01 18:29:13 -0400

    feat: separating answered and unanswered questions

--- src/components/questions.js
+++ src/components/questions.js
@@ -1,35 +1,49 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import Question, { QUESTION_ROLE_ANSWERED, QUESTION_ROLE_UNANSWERED, QUESTION_ROLE_TO_ANSWER} from './Question'
+import Question, { QUESTION_ROLE_ANSWERED, QUESTION_ROLE_UNANSWERED, QUESTION_ROLE_TO_ANSWER } from './Question'
 
 class Questions extends Component {
-  render() {
-    return (
-      <div>
-        <h3 className='center'>All Questions</h3>
-        <ul className='dashboard-list'>
-          {this.props.questionIds.map((id) => (
-            <li key={id}>
-              <Question 
-                id={id}
-                questionRole={QUESTION_ROLE_UNANSWERED}/>
-              {/*id*/}
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <h3 className='center'>Unanswered Questions</h3>
+                <ul className='dashboard-list'>
+                    {this.props.unansweredQuestionIds.map((id) => (
+                        <li key={id}>
+                            <Question
+                                id={id}
+                                questionRole={QUESTION_ROLE_UNANSWERED} />
+                            {/*id*/}
+                        </li>
+                    ))}
+                </ul>
+                <h3 className='center'>Answered Questions</h3>
+                <ul className='dashboard-list'>
+                    {this.props.answeredQuestionIds.map((id) => (
+                        <li key={id}>
+                            <Question
+                                id={id}
+                                questionRole={QUESTION_ROLE_UNANSWERED} />
+                            {/*id*/}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
-function mapStateToProps ({ questions }) {
-  /*return {
-    tweetIds: Object.keys(tweets)
-      .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
-  }*/
-  return {
-    questionIds: Object.keys(questions)
-  }
+function mapStateToProps({ authedUser, questions  }) {
+
+    return {
+        questionIds: Object.keys(questions),
+        answeredQuestionIds: Object.keys(questions)
+            .filter((questionId) => questions[questionId].optionOne.votes.includes(authedUser)
+                                        || questions[questionId].optionTwo.votes.includes(authedUser)),
+        unansweredQuestionIds: Object.keys(questions)
+        .filter((questionId) => !questions[questionId].optionOne.votes.includes(authedUser)
+                                    && !questions[questionId].optionTwo.votes.includes(authedUser))
+    }
 }
 
 export default connect(mapStateToProps)(Questions)
