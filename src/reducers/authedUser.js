61ed8e31424be67f579ee0e17855f7598dd284a7
Raymond Kalonji <raymondkalonji@gmail.com>
2020-06-02 10:07:01 -0400

    style: formatting

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -1,12 +1,12 @@
 import { SET_AUTHED_USER, CLEAR_AUTHED_USER } from '../actions/authedUser'
 
-export default function authedUser (state = null, action) {
+export default function authedUser(state = null, action) {
     switch (action.type) {
-        case SET_AUTHED_USER :
+        case SET_AUTHED_USER:
             return action.authedUser
         case CLEAR_AUTHED_USER:
             return ''
-        default :
+        default:
             return state
-  }
+    }
 }
